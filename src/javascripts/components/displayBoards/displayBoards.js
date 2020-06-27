a851f9ed4e816b2dd967b29a89364b55e1bd98f7
Matt Logan <mlogan5212@gmail.com>
2020-06-27 13:00:34 -0500

    display welcome message and user boards when user logs in

---
+++ src/javascripts/components/displayBoards/displayBoards.js
@@ -0,0 +1,27 @@
+import './displayBoards.scss';
+import firebase from 'firebase/app';
+import 'firebase/auth';
+import boardData from '../../helpers/data/boardData';
+import utils from '../../helpers/utils';
+
+const displayBoards = () => {
+  const getUserName = firebase.auth().currentUser.displayName;
+  boardData.getBoards()
+    .then((boards) => {
+      let domString = `
+        <h2 class=userHeader><span class="welcome">Welcome,</span> ${getUserName}!</h2>
+        <div class="boardContainer">
+      `;
+      boards.forEach((board) => {
+        domString += `
+          <div class="boardButton">
+          <button class="btn btn-danger boardbtn">${board.title}</button>
+          </div>`;
+      });
+      domString += '</div>';
+      utils.printToDom('#boards', domString);
+    })
+    .catch((err) => console.error('bork', err));
+};
+
+export default { displayBoards };
